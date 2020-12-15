0cb91a28e993d0d941fb213fac6da7268ff94553
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2020-12-15 23:38:58 +0900

    1215

--- autoGmail.js
+++ autoGmail.js
@@ -1,125 +1,75 @@
+var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
+var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
+var lastrow = sheet.getLastRow();
+var values = sheet.getDataRange().getValues();
 
-function work(fn){
-  var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
-  var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
-  var lastrow = sheet.getLastRow();
-  var values = sheet.getDataRange().getValues();
-  var date = new Date();
-  console.log(range)
-  console.log(date)
+let today = new Date();
 
-// function findRow(sheet,val,col){
-//   var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
-//   var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
-//   var lastrow = sheet.getLastRow();
-//   var values = lastrow.getDataRange().getValues();
-//   var date = new Date();
-//     for(var i=1;i<=31;i++){
-//       if(sheet.getRange(1,i+1).getValue === date){
-        switch(values[2][i]){
-          case "出勤":
-            return 1;
-            break;
-          case "公休":
-            return 2;
-            break;
-          default:
-
-        }
-//       }
-//     }
-//   }
-//   console.log(findRow());
-}
-
-
-function findRow(sheet,val,col){
-  var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
-  var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
-  var lastrow = sheet.getLastRow();
-  var values = sheet.getDataRange().getValues();
-  var date = new Date();
-    for(var i=1; i<=values.length;i++){
-            if(values[i][col-1] === val){
-              return i+1;
-      }
-      return 0
-    }
-};
-
-findRow(sheet,date,);
-
-
-
-function findAll(){
-  var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
-  var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
-  var lastrow = sheet.getLastRow();
-  var values = sheet.getDataRange().getValues();
-  var date = new Date();
-
-  var textFinder = sheet.createTextFinder(date);
-  var ranges = textFinder.findAll();
-
-
-  for ( var i = 0; i < ranges.length; i++ ) {
-    Logger.log('A1Notation = ' + ranges[i].getA1Notation()) ;
-}
-
-    // for(var i=2;i<=32;i++){
-    //         if(sheet.getRange(1,i).getValue() === date){
-    //           return i;
-    //   }
-    //   return 0
-    // }
-  };
-
-
-
+today = Utilities.formatDate(today,"JST", "yyyy/MM/dd");
 
 
 function createDraft() {
-  
-  const values = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
-  const to = values[1][1]; 
-  const subject = values[3][1];
 
-  const option = {
-  cc:values[2][1]
-  };
+const values = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
+const to = values[1][1]; 
+const subject = values[3][1];
 
-  let body = '';
+const option = {
+cc:values[2][1]
+};
 
-  body += values[4][1] ;
-  body += values[5][1] ;
-  body += values[6][1] ;
-  body += values[7][1] ;
-  body += values[8][1] ;
-  body += values[9][1] ;
-  body += values[10][1] ;
-  body += values[11][1] ;
-  body += values[12][1] ;
-  body += values[13][1];
+let body = '';
+
+body += values[4][1] ;
+body += values[5][1] ;
+body += values[6][1] ;
+body += values[7][1] ;
+body += values[8][1] ;
+body += values[9][1] ;
+body += values[10][1] ;
+body += values[11][1] ;
+body += values[12][1] ;
+body += values[13][1];
 
 
 
-  GmailApp.createDraft(to,subject,body,option);
+GmailApp.createDraft(to,subject,body,option);
+
 }
 
-// switch(tuesday){
- 
-//   case "出勤":
-  
-// }
 
+function createDraftOotsuka(sheet,today,col){
 
-// if(tuesday + wednesday){
-//   if(tuesday + wednesday === 1)  {
-//     tuesday = 10:30;
-//   }elseif(tuesday  wednesday === 2){
+var today = new Date();
+today = Utilities.formatDate(today,"JST", "yyyy/MM/dd");
+console.log(today)
 
-//   }
-// }
+  for(var i=1;i<=31;i++){
+          if(values[i][0] === today){
+            console.log(values[i][1]);
+               
+            switch(values[i][1]){
+                   
+                  case "出勤":
+                  console.log("作成します");
+                  createDraft();
+               　　break;
+                   
+              
+                
+                　case "研修":
+                  console.log("研修作成します");
+               　　break;
+                   
+                  case "公休":
+                  console.log("作成しません");
+                  break;
+                  default:
 
-// https://docs.google.com/spreadsheets/d/1-EeaLnQxYxlfIbZzzwRMuwW95hRNSlFxOcWNOSx1gTk/edit?usp=sharing
-
+      　　　　}
+            
+            
+          }
+    
+  }
+};
