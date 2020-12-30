20d95d482c2c212a5444d75ba31c00c92bf3b814
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2020-12-31 05:53:58 +0900

    1231

--- date.js
+++ date.js
@@ -1,1 +1,10 @@
-var spread = SpreadsheetApp.getActiveSheet();
+const values = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
+var ary = ['日','月','火','水','木','金','土'];
+
+
+for(i=1,y=0;i<=31;i++,y++){
+    values[0][0] = "2021/1/i(y)"
+    if(y=6){
+        y=0
+    }
+}
