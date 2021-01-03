bb6e8af958c3bad8eea1447401e121bcd936ddea
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-01-03 23:53:04 +0900

    0103

--- date.js
+++ date.js
@@ -3,11 +3,15 @@ function shift() {
     var ary = ['金','土','日','月','火','水','木'];
     
     
-    for(i=1,y=0,t=4;i<=31;i++,y++,t++){
+    for(i=1,t=4;i<=31;i++,t++){
         if(y>6){
             y=0
         }
-      var week = "("+ary[y]+")"
+
+      var date = new Date(2021,1,i);
+      var day = date.getDay();
+
+      var week = "("+ary[day]+")"
       values[1][t] = "2021/1/"
       values[1][t] += i
       values[1][t] += week
