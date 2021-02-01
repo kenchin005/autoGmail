73ed311574fa670a15f54487525160b364538c8a
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-02-02 00:59:00 +0900

    0202

--- autoGmail.js
+++ autoGmail.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 var spreadsheet = SpreadsheetApp.openById("197b40m1Y2WiFNEleVmFLEtVeiGZMjlYOBtE2fBDNbMM");
 var sheet = spreadsheet.getSheetByName("12月(光通信)  ");
 var lastrow = sheet.getLastRow();
@@ -14,25 +7,90 @@ var values = sheet.getDataRange().getValues();
 
 
 
+function createDraftOotsuka(){
+
+  forMatch();
+
+};
+
+
+function forMatch(){
+  var ary = ['日','月','火','水','木','金','土'];
+  var today = new Date();
+  var weekNum = today.getDay();
+  var week = '('+ary[weekNum]+')';
+  
+  today = Utilities.formatDate(today,"JST", "yyyy/MM/dd");
+  var todays = today+week
+  console.log(todays)
+  
+  /////-------------------------------------
+function workMatch(workEarly,workLate){
+
+
+  if(todays.match("火")||todays.match("木")){
+    createDraft(workEarly);
+    console.log("火木")
+  }else if(todays.match("水")||todays.match("金")){
+    if(values[i-1][2] === "出勤"){
+      createDraft(workLate);
+      console.log("水金前日出勤")
+    }else{
+      createDraft(workEarly);
+      console,log("水金前日公休")
+    }
+  
+     
+  }else{
+    createDraft(workLate);
+  }
+  
+  
+  }
+   
+        
+        
+/////----------------------------
+
+  for(var i=1;i<=31;i++){
+      if(values[i][0] === todays){
+        console.log(values[i][2]);
+          switch(values[i][2]){
+                   
+    case "出勤":
+    console.log("作成します");
+    workMatch("《出勤打刻時間》10:30","《出勤打刻時間》11:00");
+ 
+
+ 　　break;
+     
+
+  　case "研修":
+    console.log("研修作成します");
+ 　　break;
+     
+    case "公休":
+    console.log("作成しません");
+      
+      
+    break;
+    default:
+
+　　　　}
+  }
+ 
+}
+  
+
+
+
+
+}
 
 
 
 
 function createDraft(time) {
-
-  // switch(todays.match(DOW)){
-  //   case "火":
-  //   values[5][1] === "《出勤打刻時間》11:00";
-  //   break;
-  
-  //   case "水":
-  //   if()  
-    
-  // }
-//悩み中1216
-
-
-
 
 const values = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
 const to = values[1][1]; 
@@ -65,88 +123,74 @@ GmailApp.createDraft(to,subject,body,option);
 
 }
 
-function workMatch(workEarly,workLate){
 
 
-  if(todays.match("火")||todays.match("木")){
-    workEarly;
-    console.log("火木")
-  }else if(todays.match("水")||todays.match("金")){
-    if(values[i-1][2] === "出勤"){
-      workLate;
-      console.log("水金前日出勤")
-    }else{
-      workEarly;
-      console,log("水金前日公休")
-    }
-  
-     
-  }else{
-    workLate;
-  }
-  
-  
-  }
-
-
-function forMatch(onTime){
-  var ary = ['日','月','火','水','木','金','土'];
-  var today = new Date();
-  var weekNum = today.getDay();
-  var week = '('+ary[weekNum]+')';
-  
-  today = Utilities.formatDate(today,"JST", "yyyy/MM/dd");
-  var todays = today+week
-  console.log(todays)
-
-  for(var i=1;i<=31;i++){
-      if(values[i][0] === todays){
-        console.log(values[i][2]);
-  }
-
-  switch(values[i][2]){
-                   
-    case "出勤":
-    console.log("作成します");
-  
-  workMatch(createDraft("《出勤打刻時間》10:30"),createDraft("《出勤打刻時間》11:00"));
-
- 　　break;
-     
-
-  　case "研修":
-    console.log("研修作成します");
- 　　break;
-     
-    case "公休":
-    console.log("作成しません");
-    break;
-    default:
-
-　　　　}
-
- }
-}
-
-
-
-function createDraftOotsuka(){
-
-  forMatch();
-
-};
-
-
-
-
-
-//トリガー設定
+  //トリガー設定
 // function setTrigger(){
   
-//   function early(onTime){ 
+  
+  
+//   var ary = ['日','月','火','水','木','金','土'];
+//   var today = new Date();
+//   var weekNum = today.getDay();
+//   var week = '('+ary[weekNum]+')';
+  
+//   today = Utilities.formatDate(today,"JST", "yyyy/MM/dd");
+//   var todays = today+week
+//   console.log(todays)
+
+//   for(var i=1;i<=31;i++){
+//       if(values[i][0] === todays){
+//         console.log(values[i][2]);
+  
+
+//   switch(values[i][2]){
+                   
+//     case "出勤":
+//     console.log("作成します");
+    
+//       if(todays.match("火")||todays.match("木")){
+//     early();
+//     console.log("火木")
+//   }else if(todays.match("水")||todays.match("金")){
+//     if(values[i-1][2] === "出勤"){
+//       late();
+//       console.log("水金前日出勤")
+//     }else{
+//       early();
+//       console,log("水金前日公休")
+//     }
+  
+     
+//   }else{
+//     late();
+//   }
+ 
+
+//  　　break;
+     
+
+//   　case "研修":
+//     console.log("研修作成します");
+//  　　break;
+     
+//     case "公休":
+//     console.log("作成しません");
+      
+//     break;
+//     default:
+
+// 　　　　}
+
+//  }
+// }
+  
+  
+  
+//   function early(){ 
 //   const time = new Date();
 //   time.setHours(10);
-//   time.setMinutes(onTime);
+//   time.setMinutes(20);
 //   ScriptApp.newTrigger('createDraftOotsuka').timeBased().at(time).create();
 //   console.log("あーりぃだよ")
 //   }
@@ -158,17 +202,15 @@ function createDraftOotsuka(){
 //   ScriptApp.newTrigger('createDraftOotsuka').timeBased().at(time).create();
 //   console.log("れいとだよ")
 //   }
-  
-//   workMatch(early(50),late());
 
 // }
 
 //トリガー削除
-// function delTrigger(){
-//   const triggers = ScriptApp.getProjectTriggers();
-//   for(const trigger of triggers){
-//     if(trigger.getHandlerFunction() == "createDraftOotsuka"){
-//       ScriptApp.deleteTrigger(trigger);
-//     }
-//   }
-// }
+//  function delTrigger(){
+//    const triggers = ScriptApp.getProjectTriggers();
+//    for(const trigger of triggers){
+//      if(trigger.getHandlerFunction() == "createDraftOotsuka"){
+//        ScriptApp.deleteTrigger(trigger);
+//      }
+//    }
+//  }
